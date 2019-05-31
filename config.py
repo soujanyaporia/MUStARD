4b3cfb2f6f690df636d6f4ef852b5ed661bd134e
Devamanyu Hazarika <devamanyu@comp.nus.edu.sg>
2019-06-01 06:10:26 +0800

    Add speaker independent flag in config (#11)
    * Add support for bert embeddings in both target and context utterances
    
    * Add BERT features download links to README
    
    * Fix comment typo
    
    * Fix bug relating to audio file loading in dataloader
    
    * Add independent flag in config

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config:
 
     use_bert = True # if False, uses glove pooling
 
+
     use_target_text = False
     use_target_audio = False # adds audio target utterance features.
     use_target_video = False # adds video target utterance features.
@@ -72,18 +73,21 @@ class SpeakerIndependentTConfig(Config):
     svm_scale = False
     use_target_text = True
     svm_c = 10.0
+    speaker_independent = True
 
 
 class SpeakerIndependentAConfig(Config):
     svm_scale = False
     use_target_audio = True
     svm_c = 1000.0
+    speaker_independent = True
 
 
 class SpeakerIndependentVConfig(Config):
     svm_scale = False
     use_target_video = True
     svm_c = 30.0
+    speaker_independent = True
 
 
 class SpeakerIndependentTAConfig(Config):
@@ -91,6 +95,7 @@ class SpeakerIndependentTAConfig(Config):
     use_target_text = True
     use_target_audio = True
     svm_c = 500.0
+    speaker_independent = True
 
 
 class SpeakerIndependentTVConfig(Config):
@@ -98,6 +103,7 @@ class SpeakerIndependentTVConfig(Config):
     use_target_text = True
     use_target_video = True
     svm_c = 10.0
+    speaker_independent = True
 
 
 class SpeakerIndependentAVConfig(Config):
@@ -105,6 +111,7 @@ class SpeakerIndependentAVConfig(Config):
     use_target_audio = True
     use_target_video = True
     svm_c = 500.0
+    speaker_independent = True
 
 
 class SpeakerIndependentTAVConfig(Config):
@@ -113,6 +120,7 @@ class SpeakerIndependentTAVConfig(Config):
     use_target_audio = True
     use_target_video = True
     svm_c = 1000.0
+    speaker_independent = True
 
 
 def config_by_key(key=''):
